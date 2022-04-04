39090158a64cce3184cbe78ad3871060812130b8
NiyongaboEric <niyongaboeric69@gmail.com>
2022-04-04 12:34:51 +0200

    fix the challenge and make it run or ignore the this commit

--- helpers/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import loaderStyle from '../styles/loader.module.css';
-
-export const Loader: FC = () => {
-	return (
-		<div className={loaderStyle.loader}></div>
-	)
-};
